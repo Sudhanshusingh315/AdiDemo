805c5ee82e07b1e6e3325fdfa8dca08f898dc409
LeaderOfMeow <ss826063@gmail.com>
2024-02-26 00:25:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,123 @@
+import { FaceMesh } from "@mediapipe/face_mesh";
+import * as Facemesh from "@mediapipe/face_mesh";
+import * as cam from "@mediapipe/camera_utils";
+import Webcam from "react-webcam";
+import { useRef, useEffect } from "react";
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  let camera = null;
+  const connect = window.drawConnectors;
+  function onResults(result) {
+    console.log(result);
+    canvasRef.current.width = webcamRef.current.video.videoWidth;
+    canvasRef.current.height = webcamRef.current.video.videoHeight;
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      result.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    if (result.multiFaceLandmarks) {
+      for (const landmarks of result.multiFaceLandmarks) {
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_TESSELATION, {
+          color: "#C0C0C070",
+          lineWidth: 1,
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYE, {
+          color: "#FF3030",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYEBROW, {
+          color: "#FF3030",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_IRIS, {
+          color: "#FF3030",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYE, {
+          color: "#30FF30",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYEBROW, {
+          color: "#30FF30",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_IRIS, {
+          color: "#30FF30",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_FACE_OVAL, {
+          color: "#E0E0E0",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LIPS, {
+          color: "#E0E0E0",
+        });
+      }
+    }
+  }
+  useEffect(() => {
+    const faceMesh = new FaceMesh({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+      },
+    });
+    faceMesh.setOptions({
+      maxNumFaces: 1,
+      refineLandmarks: true,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+    faceMesh.onResults(onResults);
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      typeof webcamRef.current !== null
+    ) {
+      const camera = new cam.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await faceMesh.send({ image: webcamRef.current.video });
+        },
+        width: 640,
+        height: 480,
+      });
+      camera.start();
+    }
+  });
+  return (
+    <>
+      <div>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginRight: "auto",
+            marginLeft: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        ></Webcam>
+      </div>
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginRight: "auto",
+          marginLeft: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480,
+        }}
+      ></canvas>
+    </>
+  );
+}
+
+export default App;
